43c88a24d825d2bfbb56007a24dfc9de06ba2e55
495964337 <495964337@qq.com>
2023-05-26 16:12:06 +0800

    desktop

---
+++ lab4/lab4.h
@@ -0,0 +1,33 @@
+#ifndef LAB4_H
+#define LAB4_H
+#include <vector>
+
+struct Edge
+{
+    int u, v;
+    double weight;
+};
+/**
+ * @brief generate a n full graph
+ *
+ * @param n the num of point
+ * @return std::vector<Edge> the graph
+ */
+std::vector<Edge> generateRandomGraph(int n);
+
+/**
+ * @brief kruskal to compute min-generate tree
+ *
+ * @param graph the graph
+ * @param n
+ * @return double the tot val of mgt
+ */
+double kruskal(std::vector<Edge> &graph, int n);
+
+/**
+ * @brief print the info of a graph
+ *
+ * @param graph
+ */
+void printGraph(const std::vector<Edge> &graph);
+#endif
